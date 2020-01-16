5c0f027b1610d93fb60eb87df517be9fe03f2506
EdwardGoomba <edward@dock90.io>
2020-01-16 00:52:24 -0500

    Setup items, item components, query items

---
+++ frontend/components/Item.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Link from 'next/link'
+import Title from './styles/Title'
+import ItemStyles from './styles/ItemStyles'
+import PriceTag from './styles/PriceTag'
+import formatMoney from '../lib/formatMoney'
+
+class Item extends Component {
+  static propTypes = {
+    item: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired
+    })
+  }
+  render() {
+    const {
+      item: {
+        image,
+        title,
+        price,
+        description,
+        id
+      }
+    } = this.props
+    return (
+      <ItemStyles>
+        {image && <img src={image} alt={title} />}
+        <Title>
+          <Link href={{
+            pathname: '/item',
+            query: { id }
+          }}>
+            <a>{title}</a>
+          </Link>
+        </Title>
+        <PriceTag>{formatMoney(price)}</PriceTag>
+        <p>{description}</p>
+        <div className="buttonList">
+          <Link href={{
+            pathname: 'update',
+            query: { id }
+          }}>
+            <a>Edit</a>
+          </Link>
+          <button>Add To Cart</button>
+          <button>Delete</button>
+        </div>
+      </ItemStyles>
+    )
+  }
+}
+
+export default Item
